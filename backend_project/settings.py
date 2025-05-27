e341b9f85a78cf6e8ab92ffdf1e86c38db30a6d9
Ali <aliradmard5@gmail.com>
2025-05-28 02:49:15 +0330

    fixed all conflict errors and database problems. waiting to connect to front end to check phase 1 completion.

--- backend_project/settings.py
+++ backend_project/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
-from time import timedelta
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -46,7 +46,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'corsheaders',
     'accounts',
-    'users',
+    # 'users',
     'products',
     'orders',
 ]
--- env/Scripts/activate.bat
+++ env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\alira\OneDrive\Desktop\University\Term 6\Internet Engineering\restaurant\env
+set VIRTUAL_ENV=C:\Users\alira\OneDrive\Desktop\University\Term 6\Internet Engineering\restaurant\backend_project\env
 
 if not defined PROMPT set PROMPT=$P$G
 
