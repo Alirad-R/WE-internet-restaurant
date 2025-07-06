f78aea9fe6f5a597ca971320ee6f4615523af21e
Ali <aliradmard5@gmail.com>
2025-07-06 15:15:55 +0330

    final commit on the branch

--- backend_project/settings.py
+++ backend_project/settings.py
@@ -186,7 +186,7 @@ REST_FRAMEWORK = {
         'django_filters.rest_framework.DjangoFilterBackend'
     ],
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
+        'rest_framework.permissions.AllowAny',
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10
